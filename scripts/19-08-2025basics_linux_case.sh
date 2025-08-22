bcc248789a9fcb903b067e4957799c6e8ba1f0c7
ganeshshit <gsb021@nist.edu>
2025-08-22 04:35:31 +0000

    Expolore the case statementand build menubar

--- scripts/19-08-2025basics_linux_case.sh
+++ scripts/19-08-2025basics_linux_case.sh
@@ -22,3 +22,29 @@ case $distro in
 esac
 
 
+finished=0
+while [ $finished -ne 1 ]
+do
+echo "Chose Your faviorit linux distribution"
+
+	echo "1 - Arch"
+	echo "2 - CantOs"
+	echo "3 - Debian"
+	echo "4 - Mint"
+	echo "5 - Ubuntu"
+	echo "6 - Something else"
+	echo "7 - exit the script"
+read option
+
+case $option in
+	1) echo "Arch is rolling release";;
+
+	2) echo "CantOs is Popular in server";;
+	3) echo "Debian is a community distribution";;
+	4) echo "Mint is popular on both deasktop and laptop";;
+	5) echo "Ubuntu is poopular in both server and computer";;
+	6) echo "There are many Distribution are there";;
+	7) finished=1;;
+	*) echo "Chose an valide One"
+	esac
+done
